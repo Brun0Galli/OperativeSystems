ac07ae85431c80841b4229c4578d352534c2346a
dpelaez-r <0257912@up.edu.mx>
2024-03-05 16:33:38 -0600

    Primer intento API

--- ProtecyoSO/APP1/views.py
+++ ProtecyoSO/APP1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse and JsonResponse
+from .PlanificationAlgorithms import execute_planification_algorithm
 # Create your views here.
 
 def IndexView(request):
@@ -11,3 +12,12 @@ def Hla(request):
 
 def bubble_sort(request):
     return render(request, 'sorting.html')
+
+def run_planification_algorithm(request):
+    processes = request.GET.get('processes')
+    quantum = request.GET.get('quantum')
+    algorithm = request.GET.get('algorithm')
+    processes = eval(processes)
+    quantum = int(quantum)
+    completion_time, process_names = execute_planification_algorithm(algorithm, processes, quantum)
+    return JsonResponse({'completion_time': completion_time, 'process_names': process_names})
